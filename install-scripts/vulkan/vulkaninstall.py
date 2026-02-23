133e74cc138f05841bee842f642a59f1e5299032
TrentCridland <Trentcridland@gmail.com>
2026-02-23 08:25:09 -0800

    remove install executable

--- install-scripts/vulkan/vulkaninstall.py
+++ install-scripts/vulkan/vulkaninstall.py
@@ -4,8 +4,8 @@ import sys
 import subprocess
 from pathlib import Path
 
-# Vulcan version to install
-VULCAN_VERSION = "1.4.341.0"
+# vulkan version to install
+VULKAN_VERSION = "1.4.341.0"
 
 # Operating system variables
 OS_NAME : str = platform.system()
@@ -15,18 +15,18 @@ IS_MAC : bool = OS_NAME == "Darwin"
 
 # Paths
 ROOT : Path = Path("C:/") if IS_WINDOWS else Path("/")
-DOWNLOAD_PATH_WINDOWS : Path = Path("temp/vulcan.exe")
-DOWNLOAD_PATH_LINUX : Path = Path("temp/vulcan.tar.xz")
-DOWNLOAD_PATH_MAC : Path = Path("temp/vulcan.zip")
+DOWNLOAD_PATH_WINDOWS : Path = Path("temp/vulkan.exe")
+DOWNLOAD_PATH_LINUX : Path = Path("temp/vulkan.tar.xz")
+DOWNLOAD_PATH_MAC : Path = Path("temp/vulkan.zip")
 EXTRACT_PATH_MAC : Path = Path("temp/ ")
 INSTALL_PATH : Path = ROOT / "FlameSpearEngine_SDKs" / "Vulkan"
 
 # Download links
-DOWNLOAD_LINK_WINDOWS : str = f"curl -L https://sdk.lunarg.com/sdk/download/{VULCAN_VERSION}/windows/vulkansdk-windows-X64-{VULCAN_VERSION}.exe -o {DOWNLOAD_PATH_WINDOWS}"
-DOWNLOAD_LINK_LINUX : str = f"curl -L https://sdk.lunarg.com/sdk/download/{VULCAN_VERSION}/linux/vulkansdk-linux-x86_64-{VULCAN_VERSION}.tar.xz -o {DOWNLOAD_PATH_LINUX}"
-DOWNLOAD_LINK_MAC : str = f"curl -L https://sdk.lunarg.com/sdk/download/{VULCAN_VERSION}/mac/vulkansdk-macos-{VULCAN_VERSION}.zip -o {DOWNLOAD_PATH_MAC}"
+DOWNLOAD_LINK_WINDOWS : str = f"curl -L https://sdk.lunarg.com/sdk/download/{VULKAN_VERSION}/windows/vulkansdk-windows-X64-{VULKAN_VERSION}.exe -o {DOWNLOAD_PATH_WINDOWS}"
+DOWNLOAD_LINK_LINUX : str = f"curl -L https://sdk.lunarg.com/sdk/download/{VULKAN_VERSION}/linux/vulkansdk-linux-x86_64-{VULKAN_VERSION}.tar.xz -o {DOWNLOAD_PATH_LINUX}"
+DOWNLOAD_LINK_MAC : str = f"curl -L https://sdk.lunarg.com/sdk/download/{VULKAN_VERSION}/mac/vulkansdk-macos-{VULKAN_VERSION}.zip -o {DOWNLOAD_PATH_MAC}"
 
-# Commands to install vulcan
+# Commands to install vulkan
 WINDOWS_INSTALL_COMMANDS : list[str] = [
     f"{DOWNLOAD_PATH_WINDOWS} --root {INSTALL_PATH} --accept-licenses --default-answer --confirm-command install",
 ]
@@ -39,15 +39,15 @@ MAC_INSTALL_COMMANDS : list[str] = [
     f"sudo {DOWNLOAD_PATH_MAC} --root {INSTALL_PATH} --accept-licenses --default-answer --confirm-command install",
 ]
 
+# Check if we are in an admin shell (windows == admin, linux / mac = sudo)
 def is_admin():
     if os.name == "nt":
-        # Windows
         import ctypes
         return ctypes.windll.shell32.IsUserAnAdmin() != 0
     else:
-        # POSIX (Linux/macOS)
         return os.geteuid() == 0
 
+# Getter for download command
 def get_download_command() -> str:
     if IS_WINDOWS:
         return DOWNLOAD_LINK_WINDOWS
@@ -56,6 +56,7 @@ def get_download_command() -> str:
     else: # mac
         return DOWNLOAD_LINK_MAC
 
+# Getter for install commands
 def get_install_commands() -> list[str]:
     if IS_WINDOWS:
         return WINDOWS_INSTALL_COMMANDS
@@ -63,28 +64,40 @@ def get_install_commands() -> list[str]:
         return LINUX_INSTALL_COMMANDS
     else: # mac
         return MAC_INSTALL_COMMANDS
-    
+
+# Getter for download path
+def get_download_path() -> Path:
+    if IS_WINDOWS:
+        return DOWNLOAD_PATH_WINDOWS
+    elif IS_LINUX:
+        return DOWNLOAD_PATH_LINUX
+    else: # mac
+        return DOWNLOAD_PATH_MAC
+
+# Run the executable downloaded and other setup items
 def install_vulkan():
     commands = get_install_commands()
     for command in commands:
         print(f"Executing : {command}")
         subprocess.run(command, shell=True)
 
-def install_windows() -> None:
+# Removes the downloaded executable
+def cleanup_install_executable():
+    download_path : Path = get_download_path()
+    if os.path.exists(download_path):
+        os.remove(download_path)
+    if IS_MAC and os.path.exists(EXTRACT_PATH_MAC):
+        os.remove(EXTRACT_PATH_MAC)
+
+
+# Downloads the vulkan executable installer and run it
+def download_and_isntall_vulkan() -> None:
+    os.makedirs(INSTALL_PATH, exist_ok=True)
     subprocess.run(get_download_command(), shell=True)
     install_vulkan()
+    cleanup_install_executable()
 
-def install_linux() -> None:
-    subprocess.run(get_download_command(), shell=True)
-    install_vulkan()
-
-def install_mac() -> None:
-    subprocess.run(get_download_command(), shell=True)
-    install_vulkan()
-
-# -----------------------------
-# Main
-# -----------------------------
+# Main function that is imported and ran to install vulkan
 def main() -> None:
     print(f"Detected OS: {OS_NAME}")
 
@@ -92,14 +105,8 @@ def main() -> None:
         print("Not in an admin shell")
         return
 
-    os.makedirs(INSTALL_PATH, exist_ok=True)
-
-    if IS_WINDOWS:
-        install_windows()
-    elif IS_LINUX:
-        install_linux()
-    elif IS_MAC:
-        install_mac()
+    if IS_WINDOWS or IS_LINUX or IS_MAC:
+        download_and_isntall_vulkan()
     else:
         print("Unsupported OS.")
         sys.exit(1)
